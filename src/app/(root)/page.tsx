73127bb5177829d78f7f3ed477a341dd9558ab9a
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-05-20 10:20:23 +0100

    implemented authentication with github oauth and next auth v5

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>This is the home page of this application</div>
+    <div>
+      <h1 className='text-3xl'>HOME</h1>
+    </div>
   )
 }
 
