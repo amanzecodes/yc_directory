fe74a81c2118361fccf551aa890418f8ea0139f9
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-05-20 10:21:39 +0100

    pretty formatting applied

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -19,22 +19,27 @@ const Navbar = async () => {
                 <span>Create</span>
               </Link>
 
-              <form action={async() => {
-                "use server"
-                await signOut({redirectTo: "/"})
-              }}>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
                 <button type="submit">Logout</button>
               </form>
 
               <Link href={`/user/${session?.user.id}`}>
-              <span>{session?.user?.name}</span></Link>
+                <span>{session?.user?.name}</span>
+              </Link>
             </>
           ) : (
-            <form action={async() => {
-                "use server"
-                await signIn('github')
-            }}>
-                <button type="submit">Login</button>
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
             </form>
           )}
         </div>
