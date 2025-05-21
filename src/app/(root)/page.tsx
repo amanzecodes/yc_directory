0161c77db54f40ff7589c61964cb0b6ba9e723d2
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-05-21 21:51:49 +0100

    implemented new next js server form action

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,18 @@
-const Home = () => {
+import SearchForm from "../../../components/SearchForm"
+
+const Home = async({ searchParams } : {
+  searchParams: Promise<{ query?: string}>
+}) => {
+
+  const query = (await searchParams).query
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
+        </p>
+        <SearchForm query={query}/>
       </section>
     </>
   )
