2efe6655f27d9d82624230b671f75f023543167f
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-05-23 01:25:55 +0100

    done with the startup card ui implementation

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import SearchForm from "../../../components/SearchForm"
+import StartupCard from "../../../components/StartupCard"
 
 const Home = async({ searchParams } : {
   searchParams: Promise<{ query?: string}>
 }) => {
 
   const query = (await searchParams).query
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: 'Amanze'},
+    _id: 1,
+    description: 'This is a description',
+    image: "https://www.shutterstock.com/image-photo/digital-brain-circuit-ai-cocept-600nw-2498421665.jpg",
+    category: "Tech",
+    title: "The impact of Next Js 15"
+  }]
+
+
   return (
     <>
       <section className="pink_container">
@@ -13,6 +26,23 @@ const Home = async({ searchParams } : {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
         </p>
         <SearchForm query={query}/>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : (
+            'All Startups'
+          )}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index:number) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   )
